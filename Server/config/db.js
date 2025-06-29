4bcea519cb5cc32eba48aa04bad55b63c7089973
imaayushk <kumaraayush327@gmail.com>
2025-06-30 01:49:13 +0530

    added env and mongoURI

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,8 +1,11 @@
 const mongoose= require('mongoose');
-
+require('dotenv').config();
 const dbConnect= async ()=>{
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/greenStep')
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("Database Connected successfully");
         
     }
--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app= express();
 const Port=3000;
+require('dotenv').config();
 const dbConnect= require('./config/db');
 app.use(express.json());
 const cors = require("cors");
 const cookieParser= require('cookie-parser');
 app.use(cookieParser());  
-
 app.use(cors({
   origin: "https://footprism.vercel.app/", 
   methods:["get", "post", "put", "delete"],
